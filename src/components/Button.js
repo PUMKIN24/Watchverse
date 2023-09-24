c76f86b8821bc1e5e94267a49246ae3e7065f260
PUMKIN24 <jer2ymatheww@gmail.com>
2023-09-24 15:48:54 +0530

    fetching videos

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({name}) => {
+  return (
+    <div className='mr-4 bg-gray-200 p-2 rounded-lg'>
+        <button >{name}</button>
+    </div>
+  )
+}
+
+export default Button
--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import Button from './Button'
 
 const ButtonList = () => {
   return (
-    <div>ButtonList</div>
+    <div className="flex m-2 p-3 ">
+      <Button name={"All"} />
+      <Button name={"News"}  />
+      <Button name={"2024"}  />
+      <Button name={"Sports"}  />
+      <Button name={"Politics"}  />
+      <Button name={"World"}  />
+      <Button name={"Economics"}  />
+      <Button name={"Science"}  />
+      <Button name={"Wild"}  />
+      <Button name={"Cooking"}  />
+      <Button name={"Exercise"}  />
+    </div>
   )
 }
 
