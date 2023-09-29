5fc6b0a1983daf655aa10273d70ce199202e1810
PUMKIN24 <jer2ymatheww@gmail.com>
2023-09-29 18:51:41 +0530

    searchbox with debouncing

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,34 +1,100 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
-
-
+import { YOUTUBE_SEARCH_API } from "../utils/constants";
 
 const Head = () => {
-    const dispatch = useDispatch()
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false)
+//   const [showSuggestions, setShowSuggestions] = useState(false);
 
-const handleToggleMenu =()=>{
-    dispatch(toggleMenu())
-} 
+  //Debouncing in useEffect
+  useEffect(() => {
+    //Make an API call after every key press
+    //but if the diff btw 2 key press is <200ms - decline the call
+    const timer = setTimeout(() => getSearchSuggestions(), 200);
+    //At every key press it tries to clear timer
+    return () => clearTimeout(timer);
+  }, [searchQuery]);
 
-    return <div className="grid grid-flow-col p-5 m-2 shadow-lg">
-        <div className="flex col-span-1 cursor-pointer">
-            <img  onClick={()=>handleToggleMenu()} className="h-8" src="https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png?f=webp" alt="menu"  />
+  const getSearchSuggestions = async () => {
+    console.log(searchQuery);
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    setSuggestions(json[1]);
+  };
 
-            <a href="/">
-            <img className="h-8 mx-2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1024px-YouTube_Logo_2017.svg.png" alt="youtube" />
-            </a>
+  const dispatch = useDispatch();
+  const handleToggleMenu = () => {
+    dispatch(toggleMenu());
+  };
+
+  return (
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      <div className="flex col-span-1 cursor-pointer">
+
+        {/* Hamburger Icon */}
+        <img
+          onClick={() => handleToggleMenu()}
+          className="h-8"
+          src="https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png?f=webp"
+          alt="menu"
+        />
+        
+        {/* Youtube logo */}
+        <a href="/">
+          <img
+            className="h-8 mx-2"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1024px-YouTube_Logo_2017.svg.png"
+            alt="youtube"
+          />
+        </a>
+      </div>
+
+      {/* Search and text box */}
+      <div className="col-span-10">
+        <div>
+          <input
+            className="w-1/2 border border-gray-400 px-5 py-2  rounded-l-full "
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={()=>setShowSuggestions(true)}
+            onBlur={()=>setShowSuggestions(false)}
+          />
+          <button className="border border-gray-400 p-2 rounded-r-full bg-gray-100">
+            Search
+          </button>
         </div>
+        
+        {showSuggestions && (
+        <div className="fixed bg-white  w-[21rem] shadow-lg rounded-lg ">
+            <ul>
+              {suggestions.map((s) => (
+                <li key={s} className="flex py-2 hover:bg-gray-200 ">
+                  <img
+                    className="w-5 h-5 mx-2 "
+                    src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/3432420/magnifier-icon-md.png"
+                    alt="search"
+                  />
+                  {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+       </div>
 
-        <div className="col-span-10">
-            <input className="w-1/2 border border-gray-400 p-2 rounded-l-full" type="text" />
-            <button className="border border-gray-400 p-2 rounded-r-full bg-gray-100">Search</button>
-        </div>
-
-        <div className="col-span-1">
-            <img className="h-8" src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png" alt="user" />
-        </div>
+      {/* UserIcon */}
+      <div className="col-span-1">
+        <img
+          className="h-8"
+          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+          alt="user"
+        />
+      </div>
     </div>
-}
+)}
 
-export default Head;
+export default Head
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 
-const GOOGLE_API_KEY = "AIzaSyC3FxS3lTZbk2gv0mWxVuskmg7_G2TrNbI";
+const GOOGLE_API_KEY = "AIzaSyB0KyiY_TLC1eVcz5b0tqXmPsK2GevHJH8";
 
 export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" + GOOGLE_API_KEY;
 
 
+export const YOUTUBE_SEARCH_API = "https://corsproxy.io/?http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
