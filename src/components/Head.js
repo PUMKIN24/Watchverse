5e940ed580fc5785b6b46172315f937c5e6a11ee
PUMKIN24 <jer2ymatheww@gmail.com>
2023-09-29 21:23:08 +0530

    caching search

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,39 +1,65 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
 import { YOUTUBE_SEARCH_API } from "../utils/constants";
+import { cacheResults } from "../utils/searchSlice";
 
-const Head = () => {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [suggestions, setSuggestions] = useState([]);
-  const [showSuggestions, setShowSuggestions] = useState(false)
-//   const [showSuggestions, setShowSuggestions] = useState(false);
+const Head = () => { 
+    //For Search box binding
+    const [searchQuery, setSearchQuery] = useState("");
+    //for suggestions
+    const [suggestions, setSuggestions] = useState([]);
+    //for focus and blur
+    const [showSuggestions, setShowSuggestions] = useState(false);
+    //Reading cache
+    const searchCache = useSelector((store) => store.search);
+ 
+
+    const dispatch = useDispatch()
+
 
   //Debouncing in useEffect
   useEffect(() => {
     //Make an API call after every key press
     //but if the diff btw 2 key press is <200ms - decline the call
-    const timer = setTimeout(() => getSearchSuggestions(), 200);
-    //At every key press it tries to clear timer
-    return () => clearTimeout(timer);
-  }, [searchQuery]);
+    //optimizing search api calls
+    const timer = setTimeout(() => {
+        if (searchCache[searchQuery]) {
+          setSuggestions(searchCache[searchQuery]);
+        } else {
+          //Make an API call and update in cache
+          getSearchSuggestions();
+        }
+      }, 200);
+  
+      //At every key press it tries to clear timer
+      return () => {
+        clearTimeout(timer);
+      };
+    }, [searchQuery]);
 
-  const getSearchSuggestions = async () => {
+
+    //Making the api call
+    const getSearchSuggestions = async () => {
     console.log(searchQuery);
     const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
     const json = await data.json();
     setSuggestions(json[1]);
-  };
+    //Update in cache after making API call
+    dispatch(
+      cacheResults({
+        [searchQuery]: json[1],
+      }));};
 
-  const dispatch = useDispatch();
-  const handleToggleMenu = () => {
+
+    //Hamburger menu
+    const handleToggleMenu = () => {
     dispatch(toggleMenu());
-  };
+    };
 
   return (
     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
       <div className="flex col-span-1 cursor-pointer">
-
         {/* Hamburger Icon */}
         <img
           onClick={() => handleToggleMenu()}
@@ -41,7 +67,7 @@ const Head = () => {
           src="https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png?f=webp"
           alt="menu"
         />
-        
+
         {/* Youtube logo */}
         <a href="/">
           <img
@@ -60,16 +86,16 @@ const Head = () => {
             type="text"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
-            onFocus={()=>setShowSuggestions(true)}
-            onBlur={()=>setShowSuggestions(false)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
           />
           <button className="border border-gray-400 p-2 rounded-r-full bg-gray-100">
             Search
           </button>
         </div>
-        
+
         {showSuggestions && (
-        <div className="fixed bg-white  w-[21rem] shadow-lg rounded-lg ">
+          <div className="fixed bg-white  w-[21rem] shadow-lg rounded-lg ">
             <ul>
               {suggestions.map((s) => (
                 <li key={s} className="flex py-2 hover:bg-gray-200 ">
@@ -84,7 +110,7 @@ const Head = () => {
             </ul>
           </div>
         )}
-       </div>
+      </div>
 
       {/* UserIcon */}
       <div className="col-span-1">
@@ -95,6 +121,7 @@ const Head = () => {
         />
       </div>
     </div>
-)}
+  );
+};
 
-export default Head
+export default Head;
