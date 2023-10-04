998710da1f99d818be3f826b181f7063178df9a5
PUMKIN24 <jer2ymatheww@gmail.com>
2023-10-04 23:03:48 +0530

    live chat

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,37 +1,59 @@
-import React, { useEffect } from 'react'
-import ChatMessage from './ChatMessage'
-import { useDispatch, useSelector } from 'react-redux'
-import { addMessage } from '../utils/chatSlice'
-import { generateRandomName, makeRandomMessage } from '../utils/helper'
+import React, { useEffect, useState } from "react";
+import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../utils/chatSlice";
+import { generateRandomName, makeRandomMessage } from "../utils/helper";
 
 const LiveChat = () => {
+  const [liveMessage, setLiveMessage] = useState("");
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    const chatMessages = useSelector((store) => store.chat.messages)
+  const chatMessages = useSelector((store) => store.chat.messages);
 
-    useEffect(()=>{
-        const timer = setInterval(()=>{
-            //API Polling
-            dispatch(
-                addMessage({
-                    name: generateRandomName(),
-                    message: makeRandomMessage(20)
-                })
-            ) 
-        },500)
+  useEffect(() => {
+    const timer = setInterval(() => {
+      //API Polling
+      dispatch(
+        addMessage({
+          name: generateRandomName(),
+          message: makeRandomMessage(20),
+        })
+      );
+    }, 500);
 
-        return ()=> clearInterval(timer)
-    }, [])
+    return () => clearInterval(timer);
+  }, []);
   return (
-    <div className='w-full h-[300px] overflow-y-scroll flex flex-col-reverse  '>
-        {chatMessages.map((c, index)=> (
-            
-        <ChatMessage key={index} name={c.name} message={c.message} />
+    <>
+      <div className="w-full h-[300px] overflow-y-scroll flex flex-col-reverse  ">
+        {chatMessages.map((c, index) => (
+          <ChatMessage key={index} name={c.name} message={c.message} />
         ))}
+      </div>
+      <form
+        className=" w-full m-1 p-1  rounded-sm"
+        onSubmit={(e) => {e.preventDefault()
+        dispatch(addMessage({
+            name: "Jerry",
+            message: liveMessage
+        }))
+        setLiveMessage("")
+    }}
+      >
+        <input
+          className="w-4/5 border border-gray-200 px-2"
+          value={liveMessage}
+          onChange={(e) => {
+            setLiveMessage(e.target.value);
+          }}
+          placeholder=" Message here..."
+          type="text"
+        />
+        <button className="mx-2 px-2 bg-blue-200 rounded-sm">Send</button>
+      </form>
+    </>
+  );
+};
 
-    </div>
-  )
-}
-
-export default LiveChat
+export default LiveChat;
